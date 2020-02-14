9664d964af1d5782e648b5d320206b35a711b0e5
Anthony Risch <rischthefisch@gmail.com>
2020-02-14 03:21:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,12 +31,13 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] = +1 
+        artist_count[artist] += 1 
       else
         artist_count = 1 
       end
     end
-    
+    artist_count
+  end
   def self.artists
     @@artists.uniq
   end
